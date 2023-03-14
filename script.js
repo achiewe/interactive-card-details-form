2e9c62ed3aacf2686218760b3dfab2c7dba3ff88
achi mamukashvili <mk@gmail.com>
2023-03-14 19:51:54 +0400

    update

--- script.js
+++ script.js
@@ -13,35 +13,42 @@ const buttonConfirm = document.querySelector('#confirm-button');
 const cardUser = document.getElementById('jane-appleseed');
 const numberOutput = document.querySelector('.output-number');
 const cvcP = document.querySelector('.cvc-p');
-const wrongCvc = document.querySelector('.error-cvc');
+const wrongCvc = document.querySelector('.erorr-cvc');
+const divCvc = document.querySelector('#cvcc');
+const mainSection = document.querySelector('.main-section');
+const successContainer = document.querySelector('.success-container');
+const continueButton = document.getElementById('continue-button');
+const zero1 = document.getElementById('paragraph1');
+const zero2 = document.getElementById('paragraph2')
 
 
-let regEmpty = /^\s*$/; 
-let regName =/^([A-Z][a-z]+)+\s([A-Z][a-z]+)+$/;
+
+let regEmpty = /^\s*$/;
+let regName = /^([A-Z][a-z]+)+\s([A-Z][a-z]+)+$/;
 let NumberLength = 16;
 let cvcLength = 3;
-
+let slanteLine = "/";
 
 
 
 
 
 const inputWrong = (input) => {
-    if(!regName.test(input.value)){
+    if (!regName.test(input.value)) {
         input.style.border = "1px solid var(--red)";
         wrongName.style.display = "flex";
-    } else{
+    } else {
         input.style.border = "1px solid var(--light-grey)";
         wrongName.style.display = "none";
     }
-} 
+}
 
 buttonConfirm.addEventListener('click', (event) => {
     event.preventDefault()
     inputWrong(enterName);
-    if(regName.test(enterName.value)){
-    cardUser.innerHTML = enterName.value;
-}
+    if (regName.test(enterName.value)) {
+        cardUser.innerHTML = enterName.value;
+    }
 })
 
 
@@ -49,8 +56,13 @@ buttonConfirm.addEventListener('click', (event) => {
 
 
 
+
+
+
+
+
 const numberErorr = (input) => {
-    if(input.value.length < NumberLength){
+    if (input.value.length < NumberLength) {
         input.style.border = "1px solid var(--red)";
         wrongNumber.style.display = "flex";
     } else {
@@ -62,9 +74,6 @@ const numberErorr = (input) => {
 buttonConfirm.addEventListener('click', (event) => {
     event.preventDefault()
     numberErorr(inputNumber);
-    if(inputNumber.value.length < NumberLength){
-        numberOutput.innerHTML = inputNumber.value;
-    }
 })
 
 
@@ -73,8 +82,14 @@ buttonConfirm.addEventListener('click', (event) => {
 
 
 
+
+
+
+
+
+
 const cvcErorr = (input) => {
-    if(input.value.length < cvcLength) {
+    if (input.value.length < cvcLength) {
         input.style.border = "1px solid var(--red)";
         wrongCvc.style.display = "flex";
     } else {
@@ -86,10 +101,42 @@ const cvcErorr = (input) => {
 buttonConfirm.addEventListener('click', (event) => {
     event.preventDefault()
     cvcErorr(inputCvc);
-    if(inputCvc.value.length < cvcLength){
+    if (inputCvc.value > 0) {
         cvcP.innerHTML = inputCvc.value;
     }
 })
+
+
+
+
+
+
+
+
+
+const wrongMonth = (month, year) => {
+    if (regEmpty.test(month.value && year.value)) {
+        month.style.border = "1px solid var(--red)";
+        year.style.border = "1px solid var(--red)";
+        emptyMonth.style.display = "flex";
+    } else {
+        month.style.border = "1px solid var(--light-grey)";
+        year.style.border = "1px solid var(--light-grey)";
+        emptyMonth.style.display = "none";
+    }
+}
+
+buttonConfirm.addEventListener('click', (event) => {
+    event.preventDefault()
+    wrongMonth(inputMonth, inputYear);
+    if(regEmpty.test(month.value && year.value)){
+    zero1.innerHTML = inputMonth.value;
+    zero2.innerHTML = inputYear.value;
+    }
+})
+
+
+
 
 
 
@@ -103,12 +150,11 @@ const copyCardNumber = (x, y) => {
         let cardValue = x.value;
         let chunks = cardValue.match(/.{1,4}/g);
         let joinskip = chunks.join(" ");
-        y.innerHTML= joinskip;
+        y.innerHTML = joinskip;
     })
 }
 
 copyCardNumber(inputNumber, numberOutput);
-copyCardNumber (enterName, cardUser);
 
 
 
@@ -118,26 +164,96 @@ copyCardNumber (enterName, cardUser);
 
 
 
-const copyInput = (x,y) => {
+
+
+
+
+
+const copyInput = (x, y) => {
     x.addEventListener("input", () => {
-    y.innerHTML = x.value;
+        y.innerHTML = x.value;
     })
 }
 
 copyInput(inputCvc, cvcP);
+copyInput(enterName, cardUser);
+copyInput(inputMonth, zero1);
 
 
 
-// const inputEmpty = (input) => {
-//     wrongName.style.display = "none";
-//     console.log("hello");
-//     if(regEmpty.test(input.value)){
-//         emptyName.style.display = "flex";
-//         input.style.border = "1px solid var(--red)";
-//     }
-// }
 
-// buttonConfirm.addEventListener('click', (event) => {
-//     event.preventDefault()
-//     inputEmpty(enterName);
-// })
+
+
+
+
+
+
+
+
+
+
+
+const copyZero = (x, y) => {
+    x.addEventListener("input", () => {
+        y.innerHTML = "/" + x.value;
+    })
+}
+
+copyZero(inputYear, zero2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const submitSection = () => {
+    if (regName.test(enterName.value) && inputCvc.value.length === cvcLength && inputNumber.value.length === NumberLength && !regEmpty.test(inputMonth.value && inputYear.value)) {
+        mainSection.style.display = "none";
+        successContainer.style.display = "flex";
+    }
+};
+
+
+buttonConfirm.addEventListener('click', (event) => {
+    event.preventDefault()
+    submitSection();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+const continue1 = () => {
+    enterName.value = "";
+    inputCvc.value = "";
+    inputNumber.value = "";
+    inputMonth.value = "";
+    inputYear.value = "";
+    numberOutput.textContent = "0000 0000 0000 0000";
+    cvcP.textContent = "000";
+    cardUser.textContent = "JANE APPLESEED";
+    zero1.textContent = "00";
+    zero2.textContent = "/00";
+}
+
+
+continueButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    continue1();
+    successContainer.style.display = "none";
+    mainSection.style.display = "flex";
+})
